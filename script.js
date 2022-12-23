f61f84d3d34e604289192bc681cc066afef4640a
Stonehagen <steinhagen@mailbox.org>
2022-12-23 12:39:00 +0100

    changed the display of the table and the javascript

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const table = document.querySelector('.table');
 
 const myLibrary = [];
 
+const tableHeader = ['Title', 'Author', 'Pages', 'Read'];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -23,18 +25,36 @@ function addBookToLibrary() {
     titleInput.value,
     authorInput.value,
     pagesInput.value,
-    readInput.value,
+    readInput.checked ? 'yes' : 'no',
   );
   myLibrary.push(newBook);
 }
 
+function getHtmlTag(tag, insideHtml) {
+  const headingCol = document.createElement(tag);
+  headingCol.innerHTML = insideHtml;
+  return headingCol;
+}
+
+function getTitleRow() {
+  const tableRow = document.createElement('tr');
+  tableHeader.forEach((header) => {
+    tableRow.appendChild(getHtmlTag('th', header));
+  });
+  return tableRow;
+}
+
+function wipeTable() {
+  table.innerHTML = '';
+  table.appendChild(getTitleRow());
+}
+
 function displayLibrary() {
+  wipeTable();
   myLibrary.forEach((book) => {
     const tableRow = document.createElement('tr');
     Object.keys(book).forEach((key) => {
-      const tableCol = document.createElement('td');
-      tableCol.textContent = book[key];
-      tableRow.appendChild(tableCol);
+      tableRow.appendChild(getHtmlTag('td', book[key]));
     });
     table.appendChild(tableRow);
   });
@@ -44,7 +64,7 @@ function clearForm() {
   titleInput.value = '';
   authorInput.value = '';
   pagesInput.value = '';
-  readInput.value = '';
+  readInput.checked = false;
 }
 
 newBookBtn.addEventListener('click', () => {
